a4d717f85eda0cb4b0970bf45824b4d2d5d93c68
Romain Denamur <romain.denamur@gmail.com>
2018-11-07 16:37:39 +0100

    test2

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "buddy" });
+  res.send({ Hibou: "Coucou mon p'tit coeur !" });
 });
 
 const PORT = process.env.PORT || 5000;
