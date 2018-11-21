747d1ed7a20930f49ce363bacb65ee6d6d30db2f
Romain Denamur <romain.denamur@gmail.com>
2018-11-21 13:51:51 +0100

    fix for prod

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
   // like our main.js file, or main.css file
   app.use(express.static('client/build'));
   // Express will serve up the index.html file
-  // if it doesn't recognize the route
+  // if it doesn't recognize the route !
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
